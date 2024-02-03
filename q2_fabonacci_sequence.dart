5110406ae7408afee2fa149a9871b6f4bca2a6a2
NomanAhmed234 <nomanpb234@gmail.com>
2024-02-03 13:59:03 +0500

    correct q2

--- q2_fabonacci_sequence.dart
+++ q2_fabonacci_sequence.dart
@@ -3,18 +3,32 @@
 import 'dart:io';
 
 void main() {
-  List<int> fanonacci_list = [];
-  List<int> normal_list = [];
-  int? fanonacci = 0;
-  stdout.write("Enter the number:");
-  int number = int.parse(stdin.readLineSync()!);
-  for (int i = 0; i < number; i++) {
-    stdout.write("Enter the number ${i + 1}:");
-    int? number_input = int.parse(stdin.readLineSync()!);
-    normal_list.add(number_input);
-    fanonacci = fanonacci! + number_input;
-    fanonacci_list.add(fanonacci);
+  // List<int> fanonacci_list = [];
+  // List<int> normal_list = [];
+  // int? fanonacci = 0;
+  // stdout.write("Enter the number:");
+  // int number = int.parse(stdin.readLineSync()!);
+  // print(number);
+  // for (int i = 0; i < number; i++) {
+  //   stdout.write("Enter the number ${i + 1}:");
+  //   int? number_input = int.parse(stdin.readLineSync()!);
+  //   normal_list.add(number_input);
+  //   fanonacci = fanonacci! + number_input;
+  //   fanonacci_list.add(fanonacci);
+  // }
+  //  print("Normal list of numbers $normal_list.");
+  // print("Fabonacci list of $fanonacci_list");
+  List<int> fabonacci_list = [];
+  int fabonacci_number = 0;
+  print("Enter the number:");
+  int count = int.parse(stdin.readLineSync()!);
+  print(count);
+  for (int i = 0; i < count; i++) {
+    fabonacci_number = fabonacci_number + i;
+    if (fabonacci_number < count) {
+      fabonacci_list.add(fabonacci_number);
+    }
   }
-   print("Normal list of numbers $normal_list.");
-  print("Fabonacci list of numbers $fanonacci_list");
+  print("Give number is $count");
+  print(fabonacci_list);
 }
